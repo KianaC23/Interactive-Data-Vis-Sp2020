02654ded53c26d8729c0cb218e43b9983778e2fc
Kiana Carrington <kiana.carrington23@gmail.com>
2020-03-07 20:21:36 -0500

    circls change size

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -4,7 +4,7 @@
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
-  radius = 5;
+  radius = 10;
 
 /** these variables allow us to access anything we manipulate in
  * init() but need access to in draw().
@@ -139,7 +139,15 @@ function draw() {
           .on("mouseover", function(d,i){
             d3.select(this).transition()
               .duration('50')
-              .attr('opacity','.85');
+              .attr('opacity','.85')
+              .attr('r', 20);
+
+          })
+          .on("mouseout",function(d,i){
+            d3.select(this).transition()
+              .duration('50')
+              .attr('r', 10);
+
 
           })
           .call(enter =>
